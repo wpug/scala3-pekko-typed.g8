40ff1570f47689c106d6667177cd2cd136833cbc
Wiesław Pawłowski <wieslaw.pawlowski@ug.edu.pl>
2023-12-16 21:54:27 +0100

    first commit

---
+++ src/main/g8/build.sbt
@@ -0,0 +1,32 @@
+name := "$name$"
+version := "1.0.0"
+
+scalaVersion := "3.3.1"
+
+scalacOptions ++= Seq(
+   "-deprecation",         // Emit warning and location for usages of deprecated APIs.
+   "-explain",             // Explain errors in more detail.
+   "-feature",             // Emit warning and location for usages of features that should be imported explicitly.
+   "-print-lines",         // Show source code line numbers.
+   "-unchecked",           // Enable additional warnings where generated code depends on assumptions
+   "-Xfatal-warnings",     // Fail the compilation if there are any warnings.
+   "-Xmigration",          // Warn about constructs whose behavior may have changed since version.
+   "-source:3.0",
+   "-encoding", "utf8",
+)
+
+/*
+  Currently available compiler options and their meanings can be looked-up in:
+
+  https://github.com/lampepfl/dotty/blob/main/compiler/src/dotty/tools/dotc/config/ScalaSettings.scala
+*/
+
+libraryDependencies ++= {
+  val pekkoV = "1.0.2"
+  Seq(
+    "org.apache.pekko" %% "pekko-actor-typed" % pekkoV,
+    "org.apache.pekko" %% "pekko-slf4j" % pekkoV,
+    //"ch.qos.logback" % "logback-classic" % "1.3.8", // for JDK ver. < 11
+    "ch.qos.logback" % "logback-classic" % "1.4.14" // for Java ver. >= 11
+  )
+}
---
+++ src/main/g8/src/main/scala/Main.scala
@@ -0,0 +1,44 @@
+package example_typed
+
+import org.apache.pekko.actor.typed.Behavior
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.ActorSystem
+
+object Counter {
+  sealed trait Cmd
+  case object Inc extends Cmd
+  case object Dec extends Cmd
+
+  def apply(cnt: Int, min: Int = 0, max: Int = 5): Behavior[Cmd] =
+    Behaviors.receive { (ctx, msg) =>
+      msg match {
+        case Inc =>
+          if (cnt == max) {
+            ctx.log.info(s"Licznik ma już wartość maksymalną „\$max” i nie może być zwiększony")
+            Behaviors.stopped
+          } else {
+            val newCnt = cnt + 1
+            ctx.log.info(s"Zwiększamy licznik do wartości \$newCnt")
+            apply(newCnt)
+          }
+        case Dec =>
+          if (cnt == min) {
+            ctx.log.info(s"Licznik ma już wartość minimalną „\$min” i nie może być zmniejszony")
+            Behaviors.stopped
+          } else {
+            val newCnt = cnt - 1
+            ctx.log.info(s"Zmniejszamy licznik do wartości \$newCnt")
+            apply(newCnt)
+          }
+
+      }}
+}
+
+@main
+def demo: Unit = {
+  val główny: ActorSystem[Counter.Cmd] = ActorSystem(Counter(0), "counter")
+  import Counter.*
+  główny ! Counter.Inc
+  główny ! Counter.Dec
+  główny ! Counter.Dec
+}
